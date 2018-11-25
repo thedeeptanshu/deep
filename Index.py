6b8492708582c993e90a48d6589e08b96ca4333e
thedeeptanshu <45334175+thedeeptanshu@users.noreply.github.com>
2018-11-25 20:12:49 +0530

    Create Index.py

---
+++ Index.py
@@ -0,0 +1,22 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns  # visualization tool
+data = pd.read_csv('file:///Users/deeptanshuveerwani/Downloads/pokemon.csv')
+data.info()
+data.corr()
+f,ax = plt.subplots(figsize=(18, 18))
+sns.heatmap(data.corr(), annot=True, linewidths=.5, fmt= '.1f',ax=ax)
+plt.show()
+data.head(10)
+data.columns
+data.Speed.plot(kind = 'line', color = 'g',label = 'Speed',linewidth=1,alpha = 0.5,grid = True,linestyle = ':')
+data.Defense.plot(color = 'r',label = 'Defense',linewidth=1, alpha = 0.5,grid = True,linestyle = '-.')
+plt.legend(loc='upper right')     # legend = puts label into plot
+plt.xlabel('x axis')              # label = name of label
+plt.ylabel('y axis')
+plt.title('Line Plot')            # title = title of plot
+plt.show()
+
+data.Speed.plot(kind = 'hist',bins = 50,figsize = (12,12))
+plt.show()
